0b78a264363083354dd110000ac80fbd902af575
NO-B-U-G <985863084@qq.com>
2023-01-02 19:50:50 +0800

    add qq_start

---
+++ qq_start.py
@@ -0,0 +1,14 @@
+# go-cqhttp(qq机器人)启动
+#cron:11 11 11 11
+
+
+import os
+from time import sleep
+
+#const $ = new Env("qq机器人启动");
+
+if __name__ == '__main__':
+    print('启动中')
+    os.system('cd /root && ./go-cqhttp -d')
+    sleep(8)
+    print('命令执行完成')
